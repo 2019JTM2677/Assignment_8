bc48cb191c807938aac05214db8cad7a07d4bfd5
2019JTM2677 <jtm192677@iitd.ac.in>
2019-09-18 12:01:52 +0530

    2nd problem

--- ps2.py
+++ ps2.py
@@ -1,1 +1,34 @@
+# program for Problem statement-2
 
+import math
+
+  
+     
+
+def main():
+    print("Welcome to the Game!")
+    flag=1
+    while True:
+        if flag:
+            print("Player 1's chance")
+            flag=~flag
+        if not flag:
+            print("Player 2's chance")
+            flag=~flag
+
+        print("Enter the position and number to be entered: ")
+        in_string = [int(x) for x in input().split(',')]
+        position=in_string[0]
+        number=in_string[1]
+        print(position,number)
+        
+        if position not in range(1,10) or number not in range(1,10):
+            print("Please enter valid number and position")
+            exit(1)
+        col=row=3
+        tic_tac = [['\0' for i in range(col)] for j in range(row)] 
+        print(tic_tac)
+        #tic_tac[position-1]=  number        
+        
+if __name__== "__main__":
+  main()
