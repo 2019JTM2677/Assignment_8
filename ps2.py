31f99435064aa33e87d55874ac17e7fd50b7a81f
2019JTM2677 <jtm192677@iitd.ac.in>
2019-09-18 14:48:48 +0530

    2nd problem edit

--- ps2.py
+++ ps2.py
@@ -2,33 +2,41 @@
 
 import math
 
-  
-     
-
 def main():
     print("Welcome to the Game!")
+    
+    
     flag=1
-    while True:
+    player=""
+    while (True):
+        
         if flag:
+            player="Player1"
             print("Player 1's chance")
-            flag=~flag
-        if not flag:
+            flag=0
+
+        else:
+            player="Player2"
             print("Player 2's chance")
-            flag=~flag
+            flag=1
 
         print("Enter the position and number to be entered: ")
         in_string = [int(x) for x in input().split(',')]
         position=in_string[0]
         number=in_string[1]
         print(position,number)
-        
         if position not in range(1,10) or number not in range(1,10):
             print("Please enter valid number and position")
             exit(1)
-        col=row=3
-        tic_tac = [['\0' for i in range(col)] for j in range(row)] 
-        print(tic_tac)
-        #tic_tac[position-1]=  number        
+        tic_tac = [' ' for i in range(0,9)]
+        tic_tac [position-1]=  number
+        for i in range(3):
+            print(tic_tac[i*3:(i+1)*3])
+            
+        #if player =="Player1"
+
         
 if __name__== "__main__":
   main()
+        
+
